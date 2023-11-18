c88609048dd6d96600461039a68f074186c04eb9
Mambetzhusupov <150808947+Mambetzhusupov@users.noreply.github.com>
2023-11-18 18:04:56 +0600

    Add files via upload

--- project_program_for_students/js/home_works.js
+++ project_program_for_students/js/home_works.js
@@ -90,7 +90,30 @@ document.getElementById('reset').addEventListener('click', () => {
     secondsElement.textContent = '00';
     millisecondsElement.textContent = '00';
 });
+//3
+let modalShown = false;
 
+function showModal() {
+    alert("Модальное окно!");
+    modalShown = true;
+}
 
+function checkModal() {
+    const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollPosition + window.innerHeight >= document.documentElement.scrollHeight && !modalShown) {
+        showModal();
+        window.removeEventListener('scroll', checkModal);
+    }
+}
+
+window.addEventListener('scroll', checkModal);
+
+function delayedModal() {
+    alert("Модальное окно через 10 секунд!");
+    window.removeEventListener('scroll', checkModal);
+}
+
+setTimeout(delayedModal, 10000);
 
 
