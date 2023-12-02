2e9afc5d10ddd7a733765e0bb4d18a0fa2468654
Mambetzhusupov <150808947+Mambetzhusupov@users.noreply.github.com>
2023-12-02 18:47:21 +0600

    Add files via upload

--- project_program_for_students/js/lesson.js
+++ project_program_for_students/js/lesson.js
@@ -119,3 +119,44 @@ function convertCurrency(fromCurrency) {
         usdInput.value = (eurValue / somToEurRate / somToUsdRate).toFixed(2);
     }
 }
+//CARDS
+const cards = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+
+let count = 1;
+
+function loadCardData(cardNumber) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${cardNumber}`)
+.then(response => response.json())
+        .then(data => {
+            cards.innerHTML = `
+                <p>${data.title}</p>
+                <p style="color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+                <span>${data.id}</span>
+            `;
+        });
+}
+
+function updateCard(direction) {
+    count += direction;
+    if (count > 200) {
+        count = 1;
+    } else if (count < 1) {
+        count = 200;
+    }
+    loadCardData(count);
+}
+
+loadCardData(count);
+
+btnNext.addEventListener('click', () => updateCard(1));
+btnPrev.addEventListener('click', () => updateCard(-1));
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+//await
+
